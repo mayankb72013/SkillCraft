7d18f43a9adc7e353514bd7af377fa293da7e2b0
Mayank Balmoori <balmoorimayank@gmail.com>
2024-10-28 23:48:44 +0530

    Added the mongoose db and connect it to the database. Added the JWT

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+const mongoose = require('mongoose');
+mongoose.connect("mongodb+srv://balmoorimayank:g0M5rOQ5CqgYUgpF@cluster0.u1quk.mongodb.net/SKILLCRAFT");
+
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = "sodinrlwnmamipsOSDNFSLENFOSD1 23102934832095@#$%^&*&^";
 
 // User-side
 app.get("/",function (req,res){
@@ -37,6 +42,6 @@ app.post("/addCourseContent",function(req,res){
 
 })
 app.delete("/deleteCourse",function (req,res){
-    
+
 })
 app.listen(3000);
