7f77a87400c33b15b32832d375d9cad542f23f8c
Mayank Balmoori <balmoorimayank@gmail.com>
2024-10-28 23:22:29 +0530

    Added the basic express Skeleton for the User/Customer

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+
+// User-side
+app.get("/",function (req,res){
+
+})
+
+app.post("/signup",function (req,res){
+    
+})
+app.post("/signin",function (req,res){
+
+})
+app.post("/courses",function (req,res){
+
+})
+app.post("/purchaseCourse",function (req,res){
+
+})
+app.post("/usersCourses",function (req,res){
+
+})
+
+
+app.listen(3000);
