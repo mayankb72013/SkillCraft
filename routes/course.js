5b3abd2140e21076d0995adb3800de507bf71a6a
Mayank Balmoori <balmoorimayank@gmail.com>
2024-10-29 23:05:15 +0530

    Added the logic for the admin/course/delete endpoint

--- routes/course.js
+++ routes/course.js
@@ -5,61 +5,118 @@ const { Course } = require('../db');
 
 const { z } = require('zod');
 
-courseRouter.post("/viewAll",function (req,res){
-    
+courseRouter.post("/viewAll", function (req, res) {
+
 })
 const { userAuth } = require('../middlewares/user');
-courseRouter.post("/purchase",userAuth,function (req,res){
-    
+courseRouter.post("/purchase", userAuth, function (req, res) {
+
 })
 
 const { adminAuth } = require('../middlewares/admin');
-courseRouter.post("/create",adminAuth,async function (req,res){
-     const creatorId = req.creatorId;
-     const title = req.body.title;
-     const description = req.body.description;
-     const price = req.body.price;
-     const imageUrl = req.body.imageUrl;
+courseRouter.post("/create", adminAuth, async function (req, res) {
+    const creatorId = req.creatorId;
+    const title = req.body.title;
+    const description = req.body.description;
+    const price = req.body.price;
+    const imageUrl = req.body.imageUrl;
 
-     const inputValidation = z.object({
-        title : z.string(),
-        description : z.string(),
-        price : z.number(),
-        imageUrl : z.string().url()
+    const inputValidation = z.object({
+        title: z.string(),
+        description: z.string(),
+        price: z.number(),
+        imageUrl: z.string().url()
     })
     const inputProcessedWithSuccess = inputValidation.safeParse(req.body);
-    if(!inputProcessedWithSuccess.success){
+    if (!inputProcessedWithSuccess.success) {
         res.json({
-            error : inputProcessedWithSuccess.error.issues
+            error: inputProcessedWithSuccess.error.issues
         })
-        return ;
+        return;
     }
-    try{
-     const course = await Course.create({
-         title : title,
-         description : description,
-         price : price,
-         imageUrl : imageUrl,
-         creatorId : creatorId
-     })
-    }
-    catch (e){
-        res.json({
-            error : e
+    let errorThrown = false;
+    try {
+        const course = await Course.create({
+            title: title,
+            description: description,
+            price: price,
+            imageUrl: imageUrl,
+            creatorId: creatorId
         })
     }
+    catch (e) {
+        console.log(e);
+        errorThrown = true;
+    }
+    if (errorThrown) {
+        res.status(409).json({
+            msg: "A course with this title already exists"
+        })
+    }
+    else {
+        res.json({
+            msg: "Course has been added"
+        })
+    }
+})
+courseRouter.put("/edit", adminAuth, async function (req, res) {
 
-    res.json({
-       msg : "Course has been added"    
+})
+courseRouter.delete("/delete", adminAuth, async function (req, res) {
+    const creatorId = req.creatorId;
+    const title = req.body.title;
+    const description = req.body.description;
+    const price = req.body.price;
+    const imageUrl = req.body.imageUrl;
+
+    const inputValidation = z.object({
+        title: z.string(),
+        description: z.string(),
+        price: z.number(),
+        imageUrl: z.string().url()
     })
-})
-courseRouter.put("/edit",adminAuth,function (req,res){
-
-})
-courseRouter.delete("/delete",adminAuth,function (req,res){
+    const inputProcessedWithSuccess = inputValidation.safeParse(req.body);
+    if (!inputProcessedWithSuccess.success) {
+        res.json({
+            error: inputProcessedWithSuccess.error.issues
+        })
+        return;
+    }
+    let course;
+    let errorThrown = false;
+    try {
+        course = await Course.findOne({
+            title: title,
+            description: description,
+            price: price,
+            imageUrl: imageUrl,
+            creatorId: creatorId
+        })
+    }
+    catch (e) {
+        console.log(e);
+        errorThrown = true;
+        
+    }
+    if(errorThrown){
+        res.status(502).json({
+            error : "Database is down"
+        })
+    }
+    if (!course) {
+        res.status(404).json({
+            error: "No such course exists"
+        })
+    }
+    else{
+        await Course.deleteOne(course);
+        res.json({
+            msg: "Course has been Deleted"
+        })
+    }
 
 })
 
 module.exports = {
-    courseRouter : courseRouter
+    courseRouter: courseRouter
 }
