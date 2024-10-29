b9d745876239419b9a62b1f39bedba061553eae6
Mayank Balmoori <balmoorimayank@gmail.com>
2024-10-29 23:56:27 +0530

    Added the logic for editing a course and viewing all created courses for admins

--- routes/course.js
+++ routes/course.js
@@ -5,8 +5,8 @@ const { Course } = require('../db');
 
 const { z } = require('zod');
 
-courseRouter.post("/viewAll", function (req, res) {
-
+courseRouter.get("/viewAll", function (req, res) {
+    
 })
 const { userAuth } = require('../middlewares/user');
 courseRouter.post("/purchase", userAuth, function (req, res) {
@@ -14,6 +14,23 @@ courseRouter.post("/purchase", userAuth, function (req, res) {
 })
 
 const { adminAuth } = require('../middlewares/admin');
+courseRouter.get("/viewCourses",adminAuth,async function (req,res){
+   const creatorId = req.creatorId;
+   
+   const courses = await Course.find({
+       creatorId: creatorId
+   })
+   if(courses.length < 1){
+      res.json({
+        msg : "No courses have been created by you"
+      })
+   }
+   else{
+      res.json({
+        courses : courses
+      })
+   }
+})
 courseRouter.post("/create", adminAuth, async function (req, res) {
     const creatorId = req.creatorId;
     const title = req.body.title;
@@ -36,7 +53,7 @@ courseRouter.post("/create", adminAuth, async function (req, res) {
     }
     let errorThrown = false;
     try {
-        const course = await Course.create({
+        await Course.create({
             title: title,
             description: description,
             price: price,
@@ -60,7 +77,54 @@ courseRouter.post("/create", adminAuth, async function (req, res) {
     }
 })
 courseRouter.put("/edit", adminAuth, async function (req, res) {
+    const creatorId = req.creatorId;
+    const title = req.body.title;
+    const description = req.body.description;
+    const price = req.body.price;
+    const imageUrl = req.body.imageUrl;
+    const courseId = req.body.courseId;
 
+    const inputValidation = z.object({
+        title: z.string(),
+        description: z.string(),
+        price: z.number(),
+        imageUrl: z.string().url(),
+        courseId: z.string()
+    })
+    const inputProcessedWithSuccess = inputValidation.safeParse(req.body);
+    if (!inputProcessedWithSuccess.success) {
+        res.json({
+            error: inputProcessedWithSuccess.error.issues
+        })
+        return;
+    }
+    let errorThrown = false;
+    try {
+        await Course.updateOne({
+            _id: courseId,
+            creatorId: creatorId
+        },{
+            title: title,
+            description: description,
+            price: price,
+            imageUrl: imageUrl,
+            creatorId: creatorId
+        })
+    }
+    catch (e) {
+        console.log(e);
+        errorThrown = true;
+    }
+    if (errorThrown) {
+        res.status(409).json({
+            msg: "No such course exists with given description"
+        })
+    }
+    else {
+        res.json({
+            msg: "Course has been Edited"
+        })
+    }
 })
 courseRouter.delete("/delete", adminAuth, async function (req, res) {
     const creatorId = req.creatorId;
